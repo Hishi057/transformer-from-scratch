a8d3ba101c353dbc7746161f6e8286c5e0843075
Hishi057 <Hiratsuka512@icloud.com>
2026-02-26 17:00:49 +0900

    本格的な実装を開始した

---
+++ src/model.py
@@ -0,0 +1,54 @@
+import os
+from os.path import exists
+import torch
+import torch.nn as nn
+from torch.nn.functional import log_softmax, pad
+import math
+import copy
+import time
+from torch.optim.lr_scheduler import LambdaLR
+import pandas as pd
+import altair as alt
+from torchtext.data.functional import to_map_style_dataset
+from torch.utils.data import DataLoader
+from torchtext.vocab import build_vocab_from_iterator
+import torchtext.datasets as datasets
+import spacy
+import GPUtil
+import warnings
+from torch.utils.data.distributed import DistributedSampler
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+
+# Set to False to skip notebook execution (e.g. for debugging)
+warnings.filterwarnings("ignore")
+RUN_EXAMPLES = True
+
+# エンコーダー + デコーダー
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+    
+    # 学習用のためにencode, decodeメソッドをひとまとめにしている
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+    
+    def encode(self, src, src_mask):
+        return self.encoder(src, src_mask)
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+
+# 最後の出力
+class Generator(nn.Module):
+    def __init__(self, d_model, vocab):
+        self.proj = nn.Linear(d_model, vocab)
+    
+    def forward(self, x):
+        return log_softmax(self.proj(x), dim=-1)
