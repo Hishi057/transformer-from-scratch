f6f43e331a101e8130b3953d32c95b5e47ba43e4
Hishi057 <Hiratsuka512@icloud.com>
2026-03-02 20:50:29 +0900

    マルチヘッドまで

--- src/model.py
+++ src/model.py
@@ -54,7 +54,9 @@ class Generator(nn.Module):
     def forward(self, x):
         return log_softmax(self.proj(x), dim=-1)
 
+#
 # エンコーダー
+#
 class Encoder(nn.Module):
     def __init__(self, layer, N):
         super(Encoder, self).__init__()
@@ -78,3 +80,109 @@ class LayerNorm(nn.Module):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+# sublayerのラッパー
+class SublayerConnection(nn.Module):
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x, sublayer):
+        return x + self.dropout(self.sublayer(self.norm(x)))
+
+# エンコーダー層
+class EncoderLayer(nn.Module):
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self.self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+    
+    def forward(self, x, mask):
+        x = self.sublayer[0](x, lambda x: self.self_attn(x,x,x,mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+#
+# デコーダー
+#
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = layer
+        self.norm = LayerNorm(layer.size)
+    
+    def forward(self, x, memory, src_mask,tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+    
+    def forward(self, x, memory, src_mask, tgt_mask):
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+#
+# Attention
+#
+def subsequent_mask(size):
+    attn_shape = (1, size, size)
+    subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal = 1).type(torch.uint8)
+    return subsequent_mask == 0
+
+def attention(query, key, value, mask=None, dropout=None):
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1) / math.sqrt(d_k))
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.mathmul(p_attn, value), p_attn
+
+# マルチヘッド
+# あんまりここ理解できてないかも
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % h == 0
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+    
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+
+        query, key, value = [
+            lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+            for lin, x in zip(self.linears, (query, key, value))
+        ]
+
+        x, self_attn = attention(
+            query, key, value, mask=mask, drop=self.dropout
+        )
+
+        x = (
+            x.transpose(1,2)
+            .contiguous()
+            .view(nbatches, -1, self.h * self.d_k)
+        )
+        del query
+        del key
+        del value
+        return self.linears[-1](x)
